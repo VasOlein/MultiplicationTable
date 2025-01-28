839ecb70dcbd52c92b0d24e33b7565a296de6f1e
kab231_09 <student09@kru.ru>
2025-01-28 13:50:22 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace МАПО_Пр_01_Васильев_Олейников
+{
+    class Program
+    {
+        static void Main()
+        {
+            PrintMultiplTableUmnaz();
+        }
+
+        static void PrintMultiplTableUmnaz()
+        {
+
+        Console.WriteLine("Таблица умножения\n--------------------------------------------");
+
+            Console.Write(" |");
+            for (int i = 1; i<= 10; i++)
+            {
+                Console.Write($"{i,4}");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(new string('-', 44));
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write($"{i, 3}|");
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.Write($"{i * j,4}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
